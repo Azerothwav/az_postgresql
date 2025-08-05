dec8c94a9f05433075b90b7d36346c1966ccc2d0
Azeroth <azerothwaav@proton.me>
2025-08-05 21:24:46 +0200

    fix: change resource path calling

--- server/init.lua
+++ server/init.lua
@@ -1,23 +1,23 @@
 exports('init', function()
-    return {
-        insert = function(...)
-            return exports['az_postgresql']:insert(...)
-        end,
-        update = function(...)
-            return exports['az_postgresql']:update(...)
-        end,
-        select = function(...)
-            return exports['az_postgresql']:select(...)
-        end,
-        raw_query = function(...)
-            return exports['az_postgresql']:rawQuery(...)
-        end,
-        execute_file = function(...)
-            return exports['az_postgresql']:executeFile(...)
-        end,
-        ready = function(cb) return exports['az_postgresql']:ready(cb) end,
-        transaction = function(...)
-            return exports['az_postgresql']:transaction(...)
-        end
-    }
+  return {
+    insert = function(...)
+      return exports['az_postgresql']:insert(...)
+    end,
+    update = function(...)
+      return exports['az_postgresql']:update(...)
+    end,
+    select = function(...)
+      return exports['az_postgresql']:select(...)
+    end,
+    raw_query = function(...)
+      return exports['az_postgresql']:rawQuery(...)
+    end,
+    execute_file = function(...)
+      return exports['az_postgresql']:executeFile(..., GetInvokingResource())
+    end,
+    ready = function(cb) return exports['az_postgresql']:ready(cb) end,
+    transaction = function(...)
+      return exports['az_postgresql']:transaction(...)
+    end
+  }
 end)
--- server/modules/database/execute_file.js
+++ server/modules/database/execute_file.js
@@ -1,15 +1,15 @@
 const path = require("path");
 
-async function executeFile(filePath, params = []) {
+async function executeFile(filePath, invokingResource) {
   logger.debugger(
-    `Execute file with path ${filePath} has been called by ${GetInvokingResource()}`,
+    `Execute file with path ${filePath} has been called by ${invokingResource}`,
   );
   try {
     const absolutePath = path.resolve(
-      GetResourcePath(GetCurrentResourceName()),
+      GetResourcePath(invokingResource),
       filePath,
     );
-    const result = await connection.file(absolutePath, params);
+    const result = await connection.file(absolutePath, []);
     logger.info(`File ${filePath} executed without issues`);
     return [true, result];
   } catch (err) {
@@ -17,8 +17,8 @@ async function executeFile(filePath, params = []) {
   }
 }
 
-global.exports("executeFile", (filePath, cb) => {
-  executeFile(filePath).then(([success, resultOrError]) => {
+global.exports("executeFile", (filePath, invokingResource, cb) => {
+  executeFile(filePath, invokingResource).then(([success,  resultOrError]) => {
     if (typeof cb === "function") cb(success, resultOrError);
   });
 });
